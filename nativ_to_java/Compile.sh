12080d59d53d0bf861bff8164b37e4ac1314cf9f
savoptik <sav70792@gmail.com>
2019-11-22 15:56:30 +0300

    finished

--- nativ_to_java/Compile.sh
+++ nativ_to_java/Compile.sh
@@ -6,8 +6,9 @@
 #  Created by Артём Семёнов on 22.11.2019.
 #  Copyright © 2019 Артём Семёнов. All rights reserved.
 
-cd ./nativ_to_java/ &&
+cd ./nativ_to_java/com/baeldung/jni &&
 javac -h . HelloWorldJNI.java &&
 g++ -c -fPIC -I /Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home/include -I /Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home/include/darwin com_baeldung_jni_HelloWorldJNI.cpp -o com_baeldung_jni_HelloWorldJNI.o &&
 g++ -dynamiclib -o libnative.dylib com_baeldung_jni_HelloWorldJNI.o -lc &&
-java -cp . -Djava.library.path=/NATIVE_SHARED_LIB_FOLDER com.baeldung.jni.HelloWorldJNI
+cd .. && cd .. && cd .. &&
+java -cp . -Djava.library.path=./com/baeldung/jni com.baeldung.jni.HelloWorldJNI
